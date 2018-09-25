7f9840367adbe7008132f48031e02699caac53cf
andwong91 <andwong91@gmail.com>
2018-09-24 17:08:57 -0700

    Created Graph component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import Chartist from 'chartist';
-import ChartistGraph from 'react-chartist';
-
-
+import Graph from './Graph.js';
 
 class App extends Component {
   render() {
-
-    var data = {
-      labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
-      series: [
-        [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
-      ]
-    };
-
-    var options = {
-      high: 10,
-      low: -10,
-      axisX: {
-        labelInterpolationFnc: function(value, index) {
-          return index % 2 === 0 ? value : null;
-        }
-      }
-    };
-
-    var type = 'Bar'
-
     return (
       <div className="App">
         <header className="App-header">
@@ -38,13 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <ChartistGraph
-        type={type}
-        style={{ height: '500px' }}
-        data={data}
-        options={options}
-        />
+        <Graph />
       </div>
     );
   }
